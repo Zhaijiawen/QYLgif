9efdbb6c2d118cb9b2c0c6f8058620e90bbd30cd
pinkman <zhaijiawen@live.cn>
2018-07-06 11:00:09 +0800

    [1]

---
+++ main.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+import urllib
+import urllib.request
+import re
+import http.cookiejar
+import time
+
+# 目标对象正则表达式
+rex = r'src="(http://www.qylbbs\d*.com/attachment/.*?\.gif)"'
+# 目标url正则表达式
+rexUrl = r'href="(http://www.qylbbs\d*.com/read/\d*)"'
+# 目标名称
+name = 1
+# 目标网址
+baseUrl = "http://www.qylbbs3.com/"
+# headers
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+}
+# 获取cookie
+request = urllib.request.Request(baseUrl, headers=headers)
+cookieJar = http.cookiejar.CookieJar()
+cookieProcess = urllib.request.HTTPCookieProcessor(cookieJar)
+
+opener = urllib.request.build_opener(cookieProcess)
+opener.open(request)
+
+beginCount = input("起始页：")
+endCount = input("结束页：")
+try:
+    beginCount = int(beginCount)
+    endCount = int(endCount)
+except:
+    print("请输入数字！")
+pageCount = range(beginCount, endCount)
+for page in pageCount:
+    pageUrl = baseUrl + "thread/81/%s&type=16" % page
+
+    request = urllib.request.Request(pageUrl, headers=headers)
+    opener = urllib.request.build_opener(cookieProcess)
+    response = opener.open(request, timeout=10)
+
+    htmlResult = response.read().decode('utf-8')
+    urlResult = re.findall(rexUrl, htmlResult)
+    urlList = list(set(urlResult))
+
+    for url in urlList:
+        request = urllib.request.Request(url, headers=headers)
+        opener = urllib.request.build_opener(cookieProcess)
+        response = opener.open(request, timeout=5)
+        time.sleep(1)
+
+        urlResult = response.read().decode('utf-8')
+        gifList = re.findall(rex, urlResult)
+        for gifUrl in gifList:
+            urllib.request.urlretrieve(gifUrl, '.\pic\%s.gif' % name)
+            name = name + 1
+print("完成")

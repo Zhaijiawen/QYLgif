553ccc8324ce636b3212780c7ac37d207fee8fe7
Zhaijiawen <31505676+Zhaijiawen@users.noreply.github.com>
2018-09-15 11:07:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import urllib
 import urllib.request
 import re
 import http.cookiejar
-import time
+import time, threading
 
 # 目标对象正则表达式
-rex = r'src="(http://www.qylbbs\d*.com/attachment/.*?\.gif)"'
+rex = r'src="(http://www.\w*.com/attachment/.*?\.gif)"'
 # 目标url正则表达式
-rexUrl = r'href="(http://www.qylbbs\d*.com/read/\d*)"'
+rexUrl = r'href="(http://www.\w*.com/read/\d{6})"'
 # 目标名称
 name = 1
+# name锁
+nameLock = threading.Lock();
 # 目标网址
 baseUrl = ""
 # headers
@@ -25,43 +27,51 @@ cookieProcess = urllib.request.HTTPCookieProcessor(cookieJar)
 opener = urllib.request.build_opener(cookieProcess)
 opener.open(request)
 
-beginCount = input("起始页：")
-endCount = input("结束页：")
-try:
-    beginCount = int(beginCount)
-    endCount = int(endCount)
-except:
-    print("请输入数字！")
-pageCount = range(beginCount, endCount)
-for page in pageCount:
-    pageUrl = baseUrl + "thread/81/%s&type=16" % page
+
+def main():
+    beginCount = input("起始页：")
+    endCount = input("结束页：")
     try:
+        beginCount = int(beginCount)
+        endCount = int(endCount)
+    except:
+        print("请输入数字！")
+    pageCount = range(beginCount, endCount)
+    for page in pageCount:
+        pageUrl = baseUrl + "thread/81/%s&type=16" % page
+
         request = urllib.request.Request(pageUrl, headers=headers)
         opener = urllib.request.build_opener(cookieProcess)
         response = opener.open(request, timeout=10)
 
         htmlResult = response.read().decode('utf-8')
         urlResult = re.findall(rexUrl, htmlResult)
-        urlList = list(urlResult)
-    except:
-        print(pageUrl + "访问出错")
-    for url in urlList:
-        try:
-            request = urllib.request.Request(url, headers=headers)
-            opener = urllib.request.build_opener(cookieProcess)
-            response = opener.open(request, timeout=5)
-            time.sleep(1)
+        urlList = list(set(urlResult))
 
-            urlResult = response.read().decode('utf-8')
-            gifList = re.findall(rex, urlResult)
-        except:
-            print(url + "访问出错")
-            continue
-        for gifUrl in gifList:
-            try:
-                urllib.request.urlretrieve(gifUrl, '.\pic\%s.gif' % name)
-            except:
-                print(gifUrl + "访问出错")
-                continue
+        for url in urlList:
+            threading.Thread(target=url_thread(url)).start();
+
+
+def url_thread(url):
+    request = urllib.request.Request(url, headers=headers)
+    opener = urllib.request.build_opener(cookieProcess)
+    response = opener.open(request, timeout=5)
+    time.sleep(1)
+
+    urlResult = response.read().decode('utf-8')
+    gifList = re.findall(rex, urlResult)
+
+    for gifUrl in gifList:
+        try:
+            # 获取锁
+            nameLock.acquire();
+            urllib.request.urlretrieve(gifUrl, '.\pic\%s.gif' % threading.Thread.name + "-" + name)
             name = name + 1
-print("完成")
+        finally:
+            # 释放锁
+            nameLock.release()
+
+
+if __name__ == '__main__':
+    main();
+    print("完成!");
